f7014cc6a71ebbd1be158bed60da002214239629
liushiwei <liushiwei@mat.ai>
2018-04-28 18:26:10 +0800

    初始化项目

---
+++ src/main/java/com/peanutmain/curry/CurryApplication.java
@@ -0,0 +1,12 @@
+package com.peanutmain.curry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurryApplication.class, args);
+    }
+}
---
+++ src/test/java/com/peanutmain/curry/CurryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.peanutmain.curry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CurryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
