aaeab3371acaf563286257cc54d79e79854a3c66
liushiwei <liushiwei@mat.ai>
2018-05-02 18:21:11 +0800

    enable remember-me

--- src/main/java/com/peanutmain/curry/config/WebSecurityConfig.java
+++ src/main/java/com/peanutmain/curry/config/WebSecurityConfig.java
@@ -5,7 +5,11 @@ import com.peanutmain.curry.handler.CustomAuthenticationFailureHandler;
 import com.peanutmain.curry.handler.CustomAuthenticationSuccessHandler;
 import com.peanutmain.curry.model.User;
 import com.peanutmain.curry.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.RememberMeAuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,6 +19,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.security.web.authentication.rememberme.RememberMeAuthenticationFilter;
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
+import org.springframework.security.web.savedrequest.RequestCacheAwareFilter;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -23,24 +32,62 @@ import java.util.Objects;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    public UsernamePasswordAuthenticationFilter authenticationFilter() throws Exception {
-        CustomUsernamePasswordAuthenticationFilter filter = new CustomUsernamePasswordAuthenticationFilter();
-        filter.setAuthenticationSuccessHandler(new CustomAuthenticationSuccessHandler());
-        filter.setAuthenticationFailureHandler(new CustomAuthenticationFailureHandler());
-        filter.setAuthenticationManager(authenticationManager());
-        return filter;
+    private final String key = "apache-ELK-@#E123";
+
+    @Autowired
+    private IUserService userService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(rememberMeAuthenticationProvider());
+        auth.authenticationProvider(daoAuthenticationProvider());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/api/v1/public/*").anonymous();
         http.authorizeRequests().anyRequest().authenticated();
+        http.addFilterAt(logoutFilter(), UsernamePasswordAuthenticationFilter.class);
         http.addFilterAt(authenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-        http.csrf().disable().x509().disable().httpBasic().disable().formLogin().disable();
+        http.addFilterAt(rememberMeAuthenticationFilter(), RequestCacheAwareFilter.class);
+        http.csrf().disable().x509().disable().httpBasic().disable().formLogin().disable().logout().disable();
     }
 
     @Bean
-    public UserDetailsService userDetailsService(IUserService userService) {
+    public TokenBasedRememberMeServices rememberMeServices() {
+        return new TokenBasedRememberMeServices(key, userDetailsService());
+    }
+
+    public RememberMeAuthenticationProvider rememberMeAuthenticationProvider() {
+        return new RememberMeAuthenticationProvider(key);
+    }
+
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService());
+        return provider;
+    }
+
+    public UsernamePasswordAuthenticationFilter authenticationFilter() throws Exception {
+        CustomUsernamePasswordAuthenticationFilter filter = new CustomUsernamePasswordAuthenticationFilter();
+        filter.setAuthenticationSuccessHandler(new CustomAuthenticationSuccessHandler());
+        filter.setAuthenticationFailureHandler(new CustomAuthenticationFailureHandler());
+        filter.setAuthenticationManager(authenticationManager());
+        filter.setRememberMeServices(rememberMeServices());
+        return filter;
+    }
+
+    public RememberMeAuthenticationFilter rememberMeAuthenticationFilter() throws Exception {
+        return new RememberMeAuthenticationFilter(authenticationManager(), rememberMeServices());
+    }
+
+    public LogoutFilter logoutFilter() {
+        return new LogoutFilter(new HttpStatusReturningLogoutSuccessHandler(), rememberMeServices());
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
         return (username) -> {
             User user = userService.findByUsername(username);
             if (Objects.isNull(user)) {
